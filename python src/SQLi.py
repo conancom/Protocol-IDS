b977e269895ea7ee9721027051f14e102d3fb00c
ConanCoM (Leon) <cnoanplays@gmail.com>
2022-04-24 14:48:21 +0700

    update

--- python src/SQLi.py
+++ python src/SQLi.py
@@ -1,17 +1,45 @@
-import sparkpickle
-from pyspark.ml import Pipeline
-from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.feature import HashingTF, Tokenizer
-import joblib
-import pickle
-import base64
+# import sparkpickle
+# import joblib
+# import pickle
+# import base64
+# from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils, TopicAndPartition
+
+# from kafka import KafkaProducer
 
 
+    import sys
+
+    # import os
+
+    # os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4 pyspark-shell'
+    batch_time = 10
+
+    topics = ["get"]
+    sc = SparkContext(appName="PythonSparkStreamingKafka", master="local[4]")
+    ssc = StreamingContext(sc, batch_time)
+
+    kafkaParams = {"metadata.broker.list": "10.148.0.5:9092"}
+    start = 0
+    partition = 0
+    topic = 'get'
+
+    topicPartion = TopicAndPartition(topic, partition)
+    fromOffset = {topicPartion: int(start)}
+
+    kafkaStream = KafkaUtils.createDirectStream(ssc, [topic], kafkaParams, fromOffsets=fromOffset)
+
+    kafkaStream.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
 
 # Load the model from the file
-randomForest_from_joblib = joblib.load('RandomForest.pkl')
-myvectorizer = pickle.load(open("myVectorizer", 'rb'))
-
+# randomForest_from_joblib = joblib.load('RandomForest.pkl')
+# myvectorizer = pickle.load(open("myVectorizer", 'rb'))
+'''
 
 def clean_data(input_val):
     input_val = input_val.replace('\n', '')
@@ -63,6 +91,4 @@ def predict_sqli_attack():
     elif repeat == False:
         print("closing ")
 
-
-
-predict_sqli_attack()
+'''
